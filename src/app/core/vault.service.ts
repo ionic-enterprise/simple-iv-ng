942187a5dea081d1435b78a8a4393deef9d04003
Ken Sodemann <ken@ionic.io>
2021-12-08 11:59:11 -0600

    add the basics

---
+++ src/app/core/vault.service.ts
@@ -0,0 +1,113 @@
+import { Injectable } from '@angular/core';
+import { Session } from '../models/session';
+import {
+  BrowserVault,
+  DeviceSecurityType,
+  IdentityVaultConfig,
+  Vault,
+  VaultType,
+} from '@ionic-enterprise/identity-vault';
+import { VaultFactoryService } from './vault-factory.service';
+import {BehaviorSubject} from 'rxjs';
+
+export type UnlockMode = 'Device' | 'SessionPIN' | 'NeverLock' | 'ForceLogin';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VaultService {
+  private vault: BrowserVault | Vault;
+  private session: Session;
+  private sessionKey = 'session';
+
+  private canUnlockSubject: BehaviorSubject<boolean>;
+  get canUnlock$() {
+    return this.canUnlockSubject.asObservable();
+  }
+
+  constructor(
+    vaultFactory: VaultFactoryService
+  ) {
+    const config: IdentityVaultConfig = {
+      key: 'io.ionic.simpleiv',
+      type: VaultType.SecureStorage,
+      lockAfterBackgrounded: 5000,
+      shouldClearVaultAfterTooManyFailedAttempts: true,
+      customPasscodeInvalidUnlockAttempts: 2,
+      unlockVaultOnLoad: false,
+    };
+
+    this.canUnlockSubject = new BehaviorSubject(false)
+
+    this.vault = vaultFactory.create(config);
+
+    this.vault.onLock(() => {
+      this.session = undefined;
+      this.canUnlockSubject.next(true);
+    });
+  }
+
+  async setSession(session: Session, unlockMode: UnlockMode): Promise<void> {
+    await this.setUnlockMode(unlockMode);
+    this.session = session;
+    await this.vault.setValue(this.sessionKey, session);
+  }
+
+  async clearSession(): Promise<void> {
+    this.session = undefined;
+    this.setUnlockMode('NeverLock');
+    return this.vault.clear();
+  }
+
+  async restoreSession(): Promise<Session> {
+    if (!this.session) {
+      this.session = await this.vault.getValue(this.sessionKey);
+      this.canUnlockSubject.next(false);
+    }
+    return this.session;
+  }
+
+  private async  canUnlock(): Promise<boolean> {
+    if ((await this.vault.doesVaultExist()) && (await this.vault.isLocked())) {
+      return true;
+    }
+    return false;
+  }
+
+  private setUnlockMode(unlockMode: UnlockMode): Promise<void> {
+    let type: VaultType;
+    let deviceSecurityType: DeviceSecurityType;
+
+    switch (unlockMode) {
+      case 'Device':
+        type = VaultType.DeviceSecurity;
+        deviceSecurityType = DeviceSecurityType.Both;
+        break;
+
+      case 'SessionPIN':
+        type = VaultType.CustomPasscode;
+        deviceSecurityType = DeviceSecurityType.SystemPasscode;
+        break;
+
+      case 'ForceLogin':
+        type = VaultType.InMemory;
+        deviceSecurityType = DeviceSecurityType.SystemPasscode;
+        break;
+
+      case 'NeverLock':
+        type = VaultType.SecureStorage;
+        deviceSecurityType = DeviceSecurityType.SystemPasscode;
+        break;
+
+      default:
+        type = VaultType.SecureStorage;
+        deviceSecurityType = DeviceSecurityType.SystemPasscode;
+    }
+
+    return this.vault.updateConfig({
+      ...this.vault.config,
+      type,
+      deviceSecurityType,
+    });
+  }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../core/auth.service';
+import { VaultService } from '../core/vault.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  email: string;
+  password: string;
+  canUnlock = false;
+  hasSession = false;
 
-  constructor() {}
+  constructor(private auth: AuthService, private vault: VaultService) {
+    this.vault.canUnlock$.subscribe((x) => (this.canUnlock = x));
+  }
 
+  signIn() {
+    this.auth.login(this.email, this.password).subscribe((s) => {
+      if (s.user) {
+        this.vault.setSession(s, 'Device');
+        this.hasSession = true;
+      }
+    });
+  }
+
+  signOut() {
+    this.auth.logout().subscribe(() => this.hasSession = false);
+  }
+
+  async unlock() {
+    await this.vault.restoreSession();
+    this.hasSession = true;
+  }
 }
